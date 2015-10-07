8be6d54a4cb4fe99fd153118ae92afee5fdfe85a
nlopezjimenez <nlopezjimenez@atanasoff18.danielcastelao.org>
2015-10-07 11:24:41 +0200

    Bol2_7

---
+++ src/bol2_7/Bol2_7.java
@@ -0,0 +1,27 @@
+package bol2_7;
+
+import java.util.Scanner;
+
+
+public class Bol2_7 {
+
+    
+    public static void main(String[] args) {
+        double n;
+        double i;
+        double t;
+        double resultado;
+        Scanner dato = new Scanner(System.in);
+        System.out.print("Introducir nomina :");
+        n = dato.nextDouble();
+        System.out.print("Introducir interes anual :");
+        i = dato.nextDouble();
+        i =i/1200;
+        System.out.print("Introducir tiempo :");
+        t = dato.nextDouble();
+        t = t*12;
+        System.out.print("Resultado ="+(n*Math.pow((1+i),t)*i)/((Math.pow((1+i),t)-1)));
+    }
+    
+}
+        
